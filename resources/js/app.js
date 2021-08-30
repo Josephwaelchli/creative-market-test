9f78852c0704f6c90d5c2ae516c2a5ab2e04a7e1
Joseph Waelchli <joseph.waelchli@gmail.com>
2021-08-30 16:09:49 -0500

    Add vue, jquery, and bootstrap for creating application front end. Create base view and component to build on.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 require('./bootstrap');
+
+window.Vue = require('vue').default;
+
+$(function () {
+    Vue.component('application-component', require('./components/ApplicationComponent.vue').default);
+
+    const app = new Vue({
+        el: '#app',
+    });
+});
---
+++ resources/js/components/ApplicationComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container d-flex vh-100 align-content-center justify-content-center">
+        <div class="row align-content-center justify-content-center">
+            <div class="col-md-8">
+                <div class="card align-middle">
+                    <div class="card-body">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+name: "ApplicationComponent"
+}
+</script>
+
+<style scoped>
+
+</style>
